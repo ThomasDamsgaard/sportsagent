d68317e6b0aed248010ed7efd55fe6b8e619414b
ThomasDamsgaard <thomas.damsgaard@hotmail.com>
2023-02-27 13:34:39 +0100

    wip

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -1,9 +1,28 @@
 <x-app-layout>
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-            @livewire('teams.update-team-name-form', ['team' => $team])
+            <div class="mt-10 sm:mt-0">
+                <x-jet-action-section>
+                    <x-slot name="title">
+                        {{ __('Recruit Talent') }}
+                    </x-slot>
+
+                    <x-slot name="description">
+                        {{ __('Post a open player position.') }}
+                    </x-slot>
+
+                    <!-- Team Member List -->
+                    <x-slot name="content">
+                        <div class="space-y-6">
+                        </div>
+                    </x-slot>
+                </x-jet-action-section>
+            </div>
+
 
             @livewire('teams.team-member-manager', ['team' => $team])
+
+            @livewire('teams.update-team-name-form', ['team' => $team])
 
             @if (Gate::check('delete', $team) && !$team->personal_team)
                 <x-jet-section-border />
