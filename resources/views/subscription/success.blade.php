0672b8d5f6b7f07f0cc7fc6ea11753f561a9233b
ThomasDamsgaard <thomas.damsgaard@hotmail.com>
2023-04-15 19:09:17 +0200

    Update success.blade.php

--- resources/views/subscription/success.blade.php
+++ resources/views/subscription/success.blade.php
@@ -1,3 +1,33 @@
 <x-app-layout>
-    paid
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div>
+                <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+                <div class="fixed inset-0 z-10 overflow-y-auto">
+                    <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                        <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                            <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                <div class="flex flex-col items-center">
+                                    <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                                        <svg class="h-6 w-6 text-green-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                                        </svg>
+                                    </div>
+                                    <div class="mt-4 text-center sm:ml-4 sm:mt-0">
+                                        <h3 class="mt-3 text-lg font-bold leading-6 text-gray-900" id="modal-title">Payment Successful</h3>
+                                        <div class="mt-2">
+                                            <p class="text-sm text-gray-500">Thanks for your purchase. Your support means a lot to us.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="px-8 py-6 flex justify-center items-center">
+                                <x-jet-button href="{{ route('dashboard') }}">Go to dashboard</x-jet-button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
