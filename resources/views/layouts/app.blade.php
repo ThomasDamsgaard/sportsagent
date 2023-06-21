db115d7ba9c6955b4071294592b88fca8825fd08
ThomasDamsgaard <thomas.damsgaard@hotmail.com>
2023-06-21 13:14:21 +0200

    microsoft clarity

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,22 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://js.stripe.com/v3/" defer></script>
 
+    <!-- Clarity -->
+    @if (app()->environment(['staging', 'production']))
+        <script type="text/javascript">
+            (function(c, l, a, r, i, t, y) {
+                c[a] = c[a] || function() {
+                    (c[a].q = c[a].q || []).push(arguments)
+                };
+                t = l.createElement(r);
+                t.async = 1;
+                t.src = "https://www.clarity.ms/tag/" + i;
+                y = l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t, y);
+            })(window, document, "clarity", "script", "hmh0j7to5a");
+        </script>
+    @endif
+
     <!-- Styles -->
     @livewireStyles
     @stack('styles')
