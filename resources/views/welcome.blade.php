a65164752ac770d3e8a563992fb261da8cd07713
ThomasDamsgaard <thomas.damsgaard@hotmail.com>
2023-07-06 09:47:53 +0200

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
                     </div>
                 </div>
                 <div class="text-center">
-                    <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Empower Your Dreams</h1>
+                    <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Empower Your Sports Dreams</h1>
                     <p class="mt-6 text-lg leading-8 text-gray-600">Get attention from teams, players and coaches from all over the world with our <span class="font-semibold italic">Virtual Sports Agent.</span></p>
                     <div class="mt-10 flex items-center justify-center gap-x-6">
                         <a href="{{ route('register') }}" type="button" class="inline-flex items-center px-4 py-2 border border-transparent tracking-widest active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition rounded-md bg-gray-600 text-base normal-case font-semibold leading-7 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600 ">Get started</a>
