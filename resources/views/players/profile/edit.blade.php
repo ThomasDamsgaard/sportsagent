06b4806323d43512d7cf16478f85ba2aa178c53c
ThomasDamsgaard <thomas.damsgaard@hotmail.com>
2023-09-14 18:09:39 +0200

    add the players story to the edit page

--- resources/views/players/profile/edit.blade.php
+++ resources/views/players/profile/edit.blade.php
@@ -25,7 +25,7 @@
                         <div class="lg:max-w-lg">
                             <h1 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Upload your attachments</h1>
                             <p class="mt-4 text-base leading-7 text-gray-700">
-                                This section is really important! Let others see your talent and journey, conveyed through your best video clips and testimonials, thus exposing your unique abilities before clubs and scouts on the platform.
+                                <strong class="font-semibold text-gray-900">This section is really important!</strong> - Let other people see your talent and journey, conveyed through your best video clips and testimonials, thus exposing your unique abilities before clubs and scouts on the platform.
                             </p>
                             <p class="mt-8 text-base font-semibold leading-7 text-indigo-600">Current Attachments</p>
                         </div>
@@ -91,6 +91,74 @@
                         <img class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]" src="https://source.unsplash.com/nbrxEedy1S0" alt="">
                     </div>
                 </div>
+            </div>
+        @endif
+
+        @if (!auth()->user()->age)
+            <div class="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+                <div class="mx-auto grid grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+
+                    <div class="px-4 lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8">
+
+                        <div class="lg:pr-4">
+                            <div class="lg:max-w-lg">
+                                <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Explore your basketball talent with OnlineSportsTalent!</h1>
+                                <p class="mt-6 text-xl leading-8 text-gray-700">
+                                    Welcome to the platform where your sports dreams may take off.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="-mt-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+                        <img class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]" src="https://source.unsplash.com/XmYSlYrupL8" alt="">
+                    </div>
+                    <div class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+                        <div class="lg:pr-4">
+                            <div class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
+
+                                <h3 class="mt-8 text-xl font-semibold tracking-tight text-gray-900">Our mission is threefold: </h3>
+                                <ul role="list" class="mt-8 space-y-8 text-gray-600">
+                                    <li class="flex gap-x-3">
+                                        @svg('heroicon-s-user-circle', 'mt-1 h-5 w-5 flex-none text-indigo-600')
+                                        <span><strong class="font-semibold text-gray-900">Share Your Story</strong><br>Your journey as an athlete is a story important to tell. Your story like you want to tell it - through OnlineSportsTalent you can do just that. Let others see your talent and journey, conveyed through your best video clips and testimonials. Your story is a powerful tool that showcases your dedication, passion, and talent, thus exposing your unique abilities before clubs and scouts.
+                                            <br>We assist you in presenting your sports talent to professional clubs and colleges and to support these organizations in discovering exceptional talents like yours.
+                                            <br>Our platform acts as a conduit, facilitating direct communication between you and the interested parties, free from any interference by OnlineSportsTalent.
+                                        </span>
+                                    </li>
+                                    <li class="flex gap-x-3">
+                                        @svg('heroicon-s-chat-bubble-oval-left-ellipsis', 'mt-1 h-5 w-5 flex-none text-indigo-600')
+                                        <span><strong class="font-semibold text-gray-900">Connect with Clubs and Associations Worldwide</strong><br>Ever dreamt of showcasing your skills to clubs and associations beyond your local scene? OnlineSportsTalent isn't just a platform; it's your bridge to a global sports network. Our platform propels your talent onto the radar of international teams, giving you opportunities that span continents. Imagine being able to connect with clubs and associations, not just in your region, but around the world. Let your talent be seen by the right people, no matter where they are. </span>
+                                    </li>
+                                    <li class="flex gap-x-3">
+                                        @svg('heroicon-s-arrow-trending-up', 'mt-1 h-5 w-5 flex-none text-indigo-600')
+                                        <span><strong class="font-semibold text-gray-900">We Grow, Together We Thrive</strong><br>
+                                            OnlineSportsTalent is a platform under construction, for the love of sport and dreams of athletes. We're committed to improvement, and you're an integral part of this journey. The more you engage and share your experience on socials, the stronger the platform becomes for everyone. So – share OnlineSportsTalent as much as possible. And the more OnlineSportsTalent will be important for clubs. Sometimes you will experience flaws when we build out the site. Rest sure that we do it on purpose to make the platform better. During our initial phase, our services are absolutely free. However, once we reach a certain threshold, we will establish a fair pricing structure based on the value that OnlineSportsTalent brings to the table. For all 1000 first movers this service is free for 6 months. After this this OnlineSportsTalent might charge $ 5 per month, with ½y subscriptions. And if you get a contract – we charge nothing. This is between you and the club / organization.
+                                        </span>
+                                    </li>
+                                </ul>
+
+                                <h3 class="mt-8 text-xl font-semibold tracking-tight text-gray-900">Roadmap:</h3>
+
+                                <p class="mt-8">
+                                    When we have sufficient athletes registered on OnlineSportsTalents we expect to upload sample contracts and connect you with specialized experts, who can guide you through the process of setting up contractual agreements. To prevent you from being lured.
+                                </p>
+                                <br>
+                                <p>
+                                    Every sport is unique and we start with Basketball. But our aspirations are to expand into icehockey, soccer/football, handball – and more depending on our successes .
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex items-center bg-white">
+                <hr class="flex-grow border-t border-gray-300">
+                <span class="flex flex-row items-center px-3 text-gray-600">
+                    @svg('heroicon-o-user', 'h-4 mr-2')
+                    Personal Information
+                </span>
+                <hr class="flex-grow border-t border-gray-300">
             </div>
         @endif
 
@@ -210,9 +278,6 @@
                                                 <input name="biography" id="biography" value="{!! $player->biography !!}" type="hidden">
                                                 <trix-editor input="biography" class="trix-content"></trix-editor>
                                             </div>
-                                            {{-- <x-trix name="biography">
-                                                {!! $player->biography !!}
-                                            </x-trix> --}}
                                         </div>
                                         <p class="mt-3 text-sm leading-6 text-gray-600">Write about yourself.</p>
                                     </div>
@@ -260,7 +325,6 @@
                                                 <input name="career" id="career" value="{!! $player->career !!}" type="hidden">
                                                 <trix-editor input="career" class="trix-content"></trix-editor>
                                             </div>
-                                            {{-- <x-trix name="career">{{ $player->career }}</x-trix> --}}
                                         </div>
                                         <p class="mt-3 text-sm leading-6 text-gray-600">Where did you play? When? What are your best results?</p>
                                     </div>
