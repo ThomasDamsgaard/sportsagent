d7680148c3610e31dee98a90b53d55321471512f
ThomasDamsgaard <thomas.damsgaard@hotmail.com>
2023-11-02 11:54:12 +0100

    salary show fix

--- resources/views/players/profile/edit.blade.php
+++ resources/views/players/profile/edit.blade.php
@@ -158,7 +158,7 @@
                                         <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Expected Yearly Salary</label>
                                         <div class="mt-2">
                                             <div class="relative">
-                                                <input type="text" name="salary" id="salary" class="block w-full rounded-md border-0 py-1.5 pr-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="At Least" value="{{ \Illuminate\Support\Str::substr($player->salary, 1) }}">
+                                                <input type="text" name="salary" id="salary" class="block w-full rounded-md border-0 py-1.5 pr-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="At Least" value="{{ $player->salary }}">
                                                 <div class="absolute inset-y-0 right-0 flex items-center">
                                                     <label for="currency" class="sr-only">Currency</label>
                                                     <select id="currency" name="currency" class="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm">
