2ee0846c7187477046dbfd4d0c08b86f35642317
ThomasDamsgaard <thomas.damsgaard@hotmail.com>
2023-11-28 07:02:47 +0100

    onboarding modal doesn't jump to the bottom

--- resources/views/livewire/onboarding-modal.blade.php
+++ resources/views/livewire/onboarding-modal.blade.php
@@ -58,8 +58,8 @@
         </section>
     </x-slot>
     <x-slot name="footer">
-        <x-secondary-button wire:click="closeModal" wire:loading.attr="disabled">
+        <a wire:click="closeModal" wire:loading.attr="disabled" class="inline-flex cursor-pointer items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-xs font-semibold uppercase tracking-widest text-gray-700 shadow-sm transition hover:text-gray-500 focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-200 active:bg-gray-50 active:text-gray-800 disabled:opacity-25">
             Dismiss
-        </x-secondary-button>
+        </a>
     </x-slot>
 </x-dialog-modal>
